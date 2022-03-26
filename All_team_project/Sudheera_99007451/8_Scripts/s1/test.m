7a64966449a551707f96b312bdee3806763fa825
SudheeraD <87614111+SudheeraDasari@users.noreply.github.com>
2022-03-27 00:53:04 +0530

    Delete test.m

--- All_team_project/Sudheera_99007451/8_Scripts/s1/test.m
+++
@@ -1,6 +0,0 @@
-[baseFileName, folder] = uigetfile('*.jpg','Select The Image'); 
-fullFileName = fullfile(folder, baseFileName);
-g = imread(fullFileName);
-Amax = max(g,[],"all");
-Amin = min(g,[],"all");
-meanval = mean2(g);
